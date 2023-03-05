b2a459cc7e6de79e68ce1851a6defd68ed352ac5
Savio Fernandes <saviof219@gmail.com>
2023-03-05 16:33:38 +0000

    segregating routes using Router & HTTP methods

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+// imports
+const express = require("express");
+//allows us to have a dotenv file with our config in it
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//sets a baseUrl for routes of goals
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
