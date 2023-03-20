1307c0ab790721cf977a36105ab5c4bcbffae560
Savio Fernandes <saviof219@gmail.com>
2023-03-20 16:10:33 +0000

    Adding userRoutes and controller

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(express.json()); //sets eveything to JSON
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes")); //sets a baseUrl for routes of goals
+app.use("/api/users", require("./routes/userRoutes")); //sets a baseUrl for routes of users
 
 app.use(errorHandler);
 
